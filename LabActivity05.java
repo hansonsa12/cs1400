de88a5907db05a01c2e71a91507c999ff4738524
hansonsa12 <35307386+hansonsa12@users.noreply.github.com>
2018-01-24 11:56:20 -0700

    Add files via upload

--- LabActivity05.java
+++ LabActivity05.java
@@ -4,10 +4,14 @@ import javax.swing.JOptionPane;
 public class LabActivity05 {
 
 	public static void main(String[] args) {
-		String day = JOptionPane.showInputDialog("");
+		String day = JOptionPane.showInputDialog("Enter a Day of the Week: ");
 		
-		if(day.equalsIgnoreCase("sunday" || "saturday")){
-				JOptionPane.showMessageDialog(null, "Day is a Weekend");
+		if(day.equals("sunday") || day.equals("saturday")){
+			JOptionPane.showMessageDialog(null, "Day is a Weekend");
+		}
+		else{
+				JOptionPane.showMessageDialog(null, "Day is a Weekday");
+				}
 	}
 	}
-}
+
