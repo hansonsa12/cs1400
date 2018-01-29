2fec60305e447096a32d7d17f649233892af0af1
hansonsa12 <35307386+hansonsa12@users.noreply.github.com>
2018-01-29 11:19:42 -0700

    Add files via upload

---
+++ OddsAndEvens.java
@@ -0,0 +1,17 @@
+package week04;
+
+public class OddsAndEvens {
+
+	public static void main(String[] args) {
+		System.out.printf("%7s%8s\n", "Odd", "Even");
+		int i = 2;
+		while (i <= 2){
+			System.out.printf("%7s%8s\n", "Odd", "Even");
+			i += 2;
+		}
+			
+//		for(int i = 2; i <= 20; i += 2) {
+//			System.out.printf("%7d%8d\n", i - 1, i);
+	}
+
+}
