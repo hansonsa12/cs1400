16bab6e2e00bb0c9f5aa1eba0a0254910a8ca061
hansonsa12 <35307386+hansonsa12@users.noreply.github.com>
2018-01-29 10:11:29 -0700

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,24 @@
+package week04;
+
+public class Loops {
+
+	public static void main(String[] args) {
+		
+//		int count = 1; //initialize variable
+//		while(count <= 5) { //starts the loop
+//			System.out.println(count);
+//		count++; // count++ = count + 1
+//		}
+//		count = 5; //resets the variable
+//		do {
+//			System.out.println(count);
+//			count--; 
+//		}while(count >= 1); //do while loop
+
+		for(int count = 1; count <= 5; count++) {
+			System.out.println("Hello"); // for loop
+		}
+	}
+
+}
+
