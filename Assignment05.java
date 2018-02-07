e1a013af564ff6a65b840b002ce57b648e371a83
hansonsa12 <35307386+hansonsa12@users.noreply.github.com>
2018-02-07 11:17:21 -0700

    Add files via upload

---
+++ Assignment05.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+
+public class Assignment05 {
+
+	public static void main(String[] args) {
+		String [] stores = {"Store 1", "Store 2", "Store 3", "Store 4",
+				"Store 5", "Store 6"};
+		
+		String input = JOptionPane.showInputDialog("Enter Sales:");
+		int sales = Integer.parseInt(input);
+		
+
+	}
+
+}
