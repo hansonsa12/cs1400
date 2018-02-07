5c32f2a818eea88017acb9a92b6bad023262ed04
hansonsa12 <35307386+hansonsa12@users.noreply.github.com>
2018-02-07 10:40:26 -0700

    Add files via upload

---
+++ Week05LabActivity03.java
@@ -0,0 +1,12 @@
+
+public class Week05LabActivity03 {
+
+	public static void main(String[] args) {
+		String str = "Chocolate";
+		char[] array = str.toCharArray();
+		for(int i = 0; i < array.length; i++)
+			System.out.println(array [i]);
+
+	}
+
+}
